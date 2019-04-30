3bb734568c21847dda647f30f281817a9ba9bddd
kzkick2nd <aruga.kazuki@gmail.com>
2019-04-30 23:19:45 +0900

    Add flag parse

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-func main(){
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
 	// 引数をパース
 	// API叩く
 	// 表示する
+	period, command, keyword := parseArgs()
+	fmt.Println(period, command, keyword)
+}
+
+func parseArgs() (period, command, keyword string) {
+	p := flag.String("past", "", "Specify time period to search for article (default=a month)")
+	flag.Parse()
+	return *p, flag.Arg(0), flag.Arg(1)
 }
