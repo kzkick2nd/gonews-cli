586d81600af77e99e07429226dfb759345e54bb6
kzkick2nd <aruga.kazuki@gmail.com>
2019-05-01 16:56:10 +0900

    Add request function for Azure News API

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 const (
@@ -78,4 +82,91 @@ func readKeywords(path string) ([]string, error) {
 	}
 
 	return keywords, nil
+}
+
+// This struct formats the answer provided by the Bing News Search API.
+type NewsAnswer struct {
+	ReadLink     string `json: "readLink"`
+	QueryContext struct {
+		OriginalQuery string `json: "originalQuery"`
+		AdultIntent   bool   `json: "adultIntent"`
+	} `json: "queryContext"`
+	TotalEstimatedMatches int `json: totalEstimatedMatches"`
+	Sort                  []struct {
+		Name       string `json: "name"`
+		ID         string `json: "id"`
+		IsSelected bool   `json: "isSelected"`
+		URL        string `json: "url"`
+	} `json: "sort"`
+	Value []struct {
+		Name  string `json: "name"`
+		URL   string `json: "url"`
+		Image struct {
+			Thumbnail struct {
+				ContentUrl string `json: "thumbnail"`
+				Width      int    `json: "width"`
+				Height     int    `json: "height"`
+			} `json: "thumbnail"`
+			Description string `json: "description"`
+			Provider    []struct {
+				Type string `json: "_type"`
+				Name string `json: "name"`
+			} `json: "provider"`
+			DatePublished string `json: "datePublished"`
+		} `json: "image"`
+	} `json: "value"`
+}
+
+func searchQuery(term string) (string, error) {
+	// Verify the endpoint URI and replace the token string with a valid subscription key.
+	const endpoint = "https://japaneast.api.cognitive.microsoft.com/bing/v7.0/news/search"
+	token := os.Getenv("AZURE_COGNITIVE_KEY")
+	searchTerm := term
+
+	// Declare a new GET request.
+	req, err := http.NewRequest("GET", endpoint, nil)
+	if err != nil {
+		return "", err
+	}
+
+	// The rest of the code in this example goes here in the main function.
+	// Add the query to the request.
+	param := req.URL.Query()
+	param.Add("q", searchTerm)
+	req.URL.RawQuery = param.Encode()
+
+	// Insert the subscription-key header.
+	req.Header.Add("Ocp-Apim-Subscription-Key", token)
+
+	// Instantiate a client.
+	client := new(http.Client)
+
+	// Send the request to Bing.
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	// Close the connection.
+	defer resp.Body.Close()
+
+	// Read the results
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	// Create a new answer object
+	ans := new(NewsAnswer)
+	err = json.Unmarshal(body, &ans)
+	if err != nil {
+		return "", err
+	}
+
+	// Iterate over search results and print the result name and URL.
+	var text string
+	for _, result := range ans.Value {
+		text += result.Name + " " + result.URL + "\n"
+	}
+	return text, nil
 }
